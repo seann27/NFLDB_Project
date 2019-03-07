d57a3f61198a96e3bdb79a00d4653b1e2fe91ce9
Sean Black <blacksek@nih.gov>
2019-03-07 11:57:45 -0500

    Added logic to breakdown plays. Need to rework scraping pipeline.

--- Database_Building/Scrapers/profootballreference/scrape_plays.py
+++ Database_Building/Scrapers/profootballreference/scrape_plays.py
@@ -23,9 +23,103 @@ def get_data(id,commented=0):
         data = soup(comment,"lxml")
     return data
 
+def analyze_play(play):
+    type = 'null'
+    location = 'null'
+    result = 'null' # yardage or 'INCOMPLETE' for incomplete pass
+    points_scored = 0
+
+    # timeout
+    if re.search(r'Timeout',play):
+        type = 'TIMEOUT'
+    elif re.search(r'challenge',play):
+        type = 'CHALLENGE'
+    elif re.search(r'Penalty',play):
+        type = 'PENALTY'
+    elif re.search(r'kicks (off|onside)',play):
+        type = 'KICKOFF'
+    elif re.search(r'punts',play):
+        type = 'PUNT'
+        result = re.match(".*(\d+) yards",play)
+        result = result.groups()[0]
+    elif re.search(r'field goal',play):
+        type = 'FG'
+        if re.search(r'no good',play):
+            result = 'NO GOOD'
+        else:
+            result = re.match(".*(\d+) yard",play)
+            result = result.groups()[0]
+            points_scored += 3
+    elif re.search(r'kicks extra point',play):
+        type = 'XP'
+        if re.search(r'no good',play):
+            result = 'NO GOOD'
+        else:
+            result = 'GOOD'
+            points_scored += 1
+    elif re.search(r'pass',play):
+        type = 'PASS'
+        if re.search(r'incomplete',play):
+            result = 'INCOMPLETE'
+        elif re.search(r'for no gain',play):
+            result = str(0)
+        elif re.search(r'for (\d+) yards',play):
+            result = re.match(".*for (\d+) yards",play)
+            result = str(result.groups()[0])
+    else:
+        type = 'RUN'
+        if re.search(r'(left|right|middle)',play):
+            location = re.match(".*((left|right)\s(end|tackle|guard)|(middle))",play)
+            location = str(location.groups()[0])
+        if re.search(r'for no gain',play):
+            result = str(0)
+        elif re.search(r'for (\d+) yards',play):
+            result = re.match(".*for (\d+) yards",play)
+            result = str(result.groups()[0])
+    return type,result,location,points_scored
+
 link = "https://www.pro-football-reference.com/boxscores/201310270den.htm"
+link = "https://www.pro-football-reference.com/boxscores/201310270min.htm"
 page_soup = get_soup(link)
 pbp_data = get_data("all_pbp",1)
 all_quarters = pbp_data.findAll("th",{"data-stat":"quarter"})
 all_timeremain = pbp_data.findAll("td",{"data-stat":"qtr_time_remain"})
-all_playdetail = pbp_data.findAll("td",{"data-stat":"detail"})
+all_downs = pbp_data.findAll("td",{"data-stat":"down"})
+all_ydstogo = pbp_data.findAll("td",{"data-stat":"yds_to_go"})
+all_locations = pbp_data.findAll("td",{"data-stat":"location"})
+all_playdetails = pbp_data.findAll("td",{"data-stat":"detail"})
+all_scores_away = pbp_data.findAll("td",{"data-stat":"pbp_score_aw"})
+all_scores_home = pbp_data.findAll("td",{"data-stat":"pbp_score_hm"})
+all_epb = pbp_data.findAll("td",{"data-stat":"exp_pts_before"})
+all_epa = pbp_data.findAll("td",{"data-stat":"exp_pts_after"})
+
+# print(len(all_quarters))
+# print(len(all_timeremain))
+# print(len(all_downs))
+# print(len(all_ydstogo))
+# print(len(all_locations))
+# print(len(all_playdetails))
+# print(len(all_scores_away))
+# print(len(all_scores_home))
+# print(len(all_epb))
+# print(len(all_epa))
+
+for idx,val in enumerate(all_playdetails):
+# idx = 38
+# for x in range(1):
+    type,result,location,points_scored = analyze_play(all_playdetails[idx].text)
+    # if playtype == 'RUN/PASS':
+    #     continue
+    if re.search(r'\D',all_quarters[idx].text):
+        all_quarters.pop(idx)
+    print(all_quarters[idx].text+" | "+
+          all_timeremain[idx].text+" | "+
+          all_downs[idx].text+" | "+
+          all_ydstogo[idx].text+" | "+
+          all_locations[idx].text+" | "+
+          all_playdetails[idx].text+" | "+
+          type+","+result+","+location+","+str(points_scored)+" | "+
+          all_scores_away[idx].text+" | "+
+          all_scores_home[idx].text+" | "+
+          all_epb[idx].text+" | "+
+          all_epa[idx].text)
