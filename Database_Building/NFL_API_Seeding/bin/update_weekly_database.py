5b63e6dbeebdecb4ca4c525befc5d859849d2420
Sean Black <blacksek@nih.gov>
2019-11-07 08:11:30 -0500

    started main script

---
+++ Database_Building/NFL_API_Seeding/bin/update_weekly_database.py
@@ -0,0 +1,22 @@
+# imports
+
+# scrape play by play from API for week
+
+# generate game summaries
+# generate individual stats
+# scrape new player data from pro-football-reference.com
+# generate skillpoint metrics
+# generate performance metrics
+# generate next week's preview metrics
+
+# scrape fantasy pros projections
+# scrape fantasy pros rankings
+# scrape DFS salaries
+# generate optimial lineups
+
+# use ML to pick best lineups
+# use ML to pick ATS, O/U results
+
+### TO DO ###
+# edit the metrics package so that skillpoints can be calculated individually?
+#   -> can this already be used, but use player stats as input?
